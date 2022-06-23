95be4f9bb3c448703ae84d04ec1d1b48b8b50b54
Lucas Souza <lucasp.sdev@gmail.com>
2022-06-23 10:01:35 -0300

    wip: env again

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: import.meta.env.VITE_VERCEL_GRAPHQL_ENDPOINT,
+  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
   cache: new InMemoryCache()
 })

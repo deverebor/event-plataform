df9ddb9029e97dceee4b25ef6d78534441875541
Lucas Souza <lucasp.sdev@gmail.com>
2022-06-22 21:09:06 -0300

    feat: 3st day

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,24 @@
 import { Video } from '../components/Video';
 import { Sidebar } from '../components/Sidebar';
 import { Header } from './../components/Header';
+import { useParams } from 'react-router-dom';
+
+type Params = {
+  slug: string;
+}
 
 export function Event() {
+  const { slug } = useParams<Params>()
+
   return(
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <Video />
+        {/* @todo:(make an component to show if the user go to an page with out slug) */}
+        { slug 
+            ? <Video lessonSlug={slug} /> 
+            : <div className='flex-1'></div> 
+        }
         <Sidebar />
       </main>
    </div>
